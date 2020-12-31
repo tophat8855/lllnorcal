c448bd33571c748b1582c92d0fdabdef260e7a10
Heather Moore-Farley <hmoorefarley@protonmail.com>
2020-12-30 16:49:50 -0800

    Whoopsie typo haha

--- src/pages/map.js
+++ src/pages/map.js
@@ -7,7 +7,7 @@ export default function Map({ data }) {
     <Layout>
       <Container>
         <p>
-          You are free to attend a any meeting, even if it is not your local group.
+          You are free to attend any meeting, even if it is not your local group.
         </p>
         <div class="responsive-iframe-container big-container">
         <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1hyxawH8A-eXKT-c06gIH5tSyNQk" width="640" height="480" title="Map of LLL Groups"></iframe>
