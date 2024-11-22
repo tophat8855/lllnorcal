c00bda84b32851d1662dbf6d276e8b4cada21df2
Heather Farley <hrfarley@gmail.com>
2024-11-22 11:21:07 -0800

    Update languages

--- src/pages/languages.js
+++ src/pages/languages.js
@@ -20,20 +20,14 @@ export default function Languages({data}) {
           <h2>Italian Italiano</h2>
           <div>Brittany - <a href="mailto: BrittanyEOC@me.com">email</a>, <a href="tel:+4159401381"> text </a></div>
 
-          <h2>Portuguese Português</h2>
-          <div>Rose - <a href="mailto: rose_ibclc@yahoo.com">email</a>, <a href="tel:+2096132332"> text </a></div>
-
           <h2>Russian русский</h2>
           <div>Ksenia - <a href="mailto: galaninaksenia@gmail.com">email</a></div>
 
           <h2>Spanish Español</h2>
           <div>Brienne - <a href="tel:+8317762832">text</a></div>
-          <div>Mary Jane - <a href="tel:+8082837139">text</a></div>
-          <div>Rachel - <a href="tel:+5305919191">text</a></div>
           <div>Glory - <a href="mailto: glorimar.canedo@gmail.com"> email</a>, <a href="tel:+8082321912">text</a></div>
           <div>Aimee - <a href="mailto: adebacker8@gmail.com">email</a>, <a href="tel:+8054445803">text</a></div>
           <div>Carolina - <a href="mailto: lllcarolinajones@gmail.com">email</a></div>
-          <div>Rose - <a href="mailto: rose_ibclc@yahoo.com">email</a>, <a href="tel:+2096132332"> text </a></div>
           <div>Nicole - <a href="mailto: npogrund@yahoo.com">email</a>, <a href="tel:+7073805177"> text </a></div>
 
           <h2>Urdu/Hindi اُردُو हिन्दी</h2>
